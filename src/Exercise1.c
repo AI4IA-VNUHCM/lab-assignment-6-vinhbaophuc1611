e5696e42e0b364063530c7831727fe2e143f6231
Vĩnh Bảo Phúc <ITITIU20127@student.hcmiu.edu.vn>
2021-11-22 15:13:17 +0700

    a

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -9,17 +9,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-void Ex1(int n){
-	//Your codes here
-	
+void Ex1(char* num){
+	int len = strlen(num);
+ 
+    char* single_digits[] = {"zero", "one", "two",   "three", "four", "five", "six", "seven", "eight", "nine" };
+ 
+    char* two_digits[] = {"", "ten", "eleven", "twelve", "thirteen",  "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+
+    char* tens_multiple[] = {"", "", "twenty", "thirty", "forty",   "fifty", "sixty",  "seventy", "eighty", "ninety" };
+ 
+    char* tens_power[] = {"hundred", "thousand" };
+
+    if (len == 1) {
+        printf("%s\n", single_digits[*num - '0']);
+        return;
+    }
+
+    while (*num != '\0') {
+        if (len >= 3) {
+            if (*num - '0' != 0) {
+                printf("%s ", single_digits[*num - '0']);
+                printf("%s ", tens_power[len - 3]);
+            }
+            --len;
+        }
+        else {
+            if (*num == '1') {
+                int sum = *num - '0' + *(num + 1) - '0';
+                printf("%s\n", two_digits[sum]);
+                return;
+            }
+            else if (*num == '2' && *(num + 1) == '0') {
+                printf("twenty\n");
+                return;
+            }
+            else {
+                int i = *num - '0';
+                printf("%s ", i ? tens_multiple[i] : "");
+                ++num;
+                if (*num != '0')
+                    printf("%s ", single_digits[*num - '0']);
+            }
+        }
+        ++num;
+    }
 }
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
-	int testcase = atoi(argv[1]);
+	char *t = argv[1];
 	
-	Ex1(testcase);
+	Ex1(t);
 	
 	return 0;
 }
--- src/Exercise4.c
+++ src/Exercise4.c
@@ -14,8 +14,12 @@ Ex:
 #include <string.h>
 
 void Ex4(char *str){
-	//Your codes here
-	
+	int k = 0;
+	for(int i = 0; i < strlen(str); ++i) {
+		if(str[i] != " "){
+			printf("%s", str[i]); k = 0;
+		}
+	}
 }
 
 int main(int argc, char *argv[]) {
