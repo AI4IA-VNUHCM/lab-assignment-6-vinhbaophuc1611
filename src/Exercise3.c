c59eb49357701929994286c396980ddd51d7b396
Vĩnh Bảo Phúc <ITITIU20127@student.hcmiu.edu.vn>
2021-11-22 16:19:28 +0700

    e

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -12,10 +12,44 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <ctype.h>
 
 void Ex3(char *str){
 	//Your codes here
-	
+	char word[20] = "";
+    char longestWord[20] = "";
+    char shortestWord[20] = "";
+    int wordIndex = 0;
+    for(int intputIndex = 0; intputIndex < strlen(c); intputIndex++)
+    {
+        while(intputIndex < strlen(c) && !isspace(c[intputIndex]) && isalnum(c[intputIndex]))
+        {
+            word[wordIndex++] = c[intputIndex];
+        }
+        if(wordIndex != 0)
+        {
+            word[wordIndex] = '\0';
+        }
+        if(strlen(longestWord) == 0)
+        {
+            strcpy(longestWord, word);
+        }
+        if(strlen(shortestWord) == 0)
+        {
+            strcpy(shortestWord, word);
+        }
+        if(strlen(word) > strlen(longestWord))
+        {
+            strcpy(longestWord, word);
+        }
+        if(strlen(word) < strlen(shortestWord))
+        {
+            strcpy(shortestWord, word);
+        }
+        wordIndex = 0;
+    }
+    printf("Longest word: \'%s\'", longestWord);
+    printf("\nShortest word: \'%s\'", shortestWord);
 }
 
 int main(int argc, char *argv[]) {
--- src/Exercise5.c
+++ src/Exercise5.c
@@ -18,8 +18,10 @@ Ex:
 void Ex5(char *str){
 	//Your codes here
 	int k = 1;
-	for(int i = 0, j = strlen(str); i < strlen(str)/2 + 1, j >= strlen(str)/2 + 1; ++i, --j){
-		if(str[i] != str[j]){
+	for(int i = 0, j = strlen(str) - 1; i <= strlen(str)/2 + 1, j >= strlen(str)/2; ++i, --j){
+		printf("%d %d\n", i, j);
+		if((str[i] == '{' && str[j] == '}') || (str[i] == '[' && str[j] == ']') || (str[i] == '(' && str[j] == ')')) continue;
+		else{
 			k = 0;
 			break;
 		}
